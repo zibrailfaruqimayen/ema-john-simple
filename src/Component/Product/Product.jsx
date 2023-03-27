d2ab4a5f49026848a8c3b80de91bc5b1f9e963f5
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-27 21:11:02 +0600

    all dome

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Product = (props) => {
-  console.log(props.product);
+  //   console.log(pro ps.product);
   const { img, name, seller, price, ratings } = props.product;
+  const handleAddToCart = props.handleAddToCart;
+
   return (
     <div className="product">
       <img src={img} alt="" />
@@ -13,7 +17,12 @@ const Product = (props) => {
         <p>Manufacturer: {seller}</p>
         <p>Rating: {ratings}Stars</p>
       </div>
-      <button className="btn-cart">Add to Cart</button>
+      <button
+        onClick={() => handleAddToCart(props.product)}
+        className="btn-cart"
+      >
+        Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
     </div>
   );
 };
