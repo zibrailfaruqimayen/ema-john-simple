38c4b58c98cbdb609f7081b64406a0ed72dae635
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-27 15:27:16 +0600

    nav bar added

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
   return (
-    <div>
-      <h2>This is a header</h2>
-    </div>
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <a href="/shop">shop</a>
+        <a href="/order">order</a>
+        <a href="/inventory">inventory</a>
+        <a href="/login">Login</a>
+      </div>
+    </nav>
   );
 };
 
