a032d318149069f23f4a99d557a91bdef24ac148
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-27 14:37:53 +0600

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Component/Header/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Header></Header>
     </div>
   );
 }
